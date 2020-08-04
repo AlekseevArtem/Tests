4c76f81fb5800d169c9f44fccbb3ad736a2f9d4b
AlekseevArtem <tema9991@mail.ru>
2020-08-04 23:21:20 +0300

    2. Инструментальные (UI) тесты [#294984]

---
+++ app/src/androidTest/java/ru/job4j/tests/ActivityForFragmentTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.tests;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class ActivityForFragmentTest {
+    @Rule
+    public ActivityTestRule<ActivityForFragment> activityTestRule =
+            new ActivityTestRule<>(ActivityForFragment.class);
+
+    @Test
+    public void checkLabelText() {
+        onView(withId(R.id.status_fragment)).check(matches(withText("...")));
+    }
+
+    @Test
+    public void checkButtonIsClickable() {
+        onView(withId(R.id.check_fragment)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void checkLabelWithError() {
+        onView(withId(R.id.check_fragment)).perform(click());
+        onView(withId(R.id.status_fragment)).check(matches(withText(R.string.validation_error)));
+    }
+
+    @Test
+    public void checkLabelSuccess() {
+        onView(withId(R.id.email_fragment)).perform(click(), replaceText("test@test.com"), pressBack());
+        onView(withId(R.id.check_fragment)).perform(click());
+        onView(withId(R.id.status_fragment)).check(matches(withText(R.string.success)));
+    }
+}
---
+++ app/src/androidTest/java/ru/job4j/tests/MainActivityTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.tests;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void checkLabelText() {
+        onView(withId(R.id.status)).check(matches(withText("...")));
+    }
+
+    @Test
+    public void checkButtonIsClickable() {
+        onView(withId(R.id.check)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void checkLabelWithError() {
+        onView(withId(R.id.check)).perform(click());
+        onView(withId(R.id.status)).check(matches(withText(R.string.validation_error)));
+    }
+
+    @Test
+    public void checkLabelSuccess() {
+        onView(withId(R.id.email)).perform(click(), replaceText("test@test.com"), pressBack());
+        onView(withId(R.id.check)).perform(click());
+        onView(withId(R.id.status)).check(matches(withText(R.string.success)));
+    }
+}
